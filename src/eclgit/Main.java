a67122f10c7ce856b945f6f8c0c25daea5b5996a
openworld42 <github@silberbauer.xyz>
2021-10-04 22:20:30 +0200

    First init

---
+++ src/eclgit/Main.java
@@ -0,0 +1,22 @@
+package eclgit;
+
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		
+		System.out.println("hello");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		new Main();
+
+	}
+
+}
